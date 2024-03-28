12a66bccb814da3960d26a661c615f09c698fa82
Nicole <nickii.cn@hotmail.com>
2024-03-28 12:24:41 -0300

    Update desafio.js

--- class_2/desafio.js
+++ class_2/desafio.js
@@ -1,4 +1,3 @@
-
 class Product {
     constructor(title, description, price, thumbnail, code, stock) {
         this.id = this.generateId();
@@ -11,13 +10,13 @@ class Product {
     }
 
     generateId() {
-        return '_' + Math.random().toString(36).length(2, 9);
+        return '_' + Math.random().toString(36).substring(2, 9);
     }
 }
 
 class ProductManager {
     constructor() {
-        this.products = []
+        this.products = [];
     }
 
     addProduct(product) {
@@ -29,28 +28,28 @@ class ProductManager {
     }
 
     getProducts() {
-        return this.products
-
+        return this.products;
     }
 
     getProductById(productId) {
-        const product = this.products.find(product => product.id === productId)
+        const product = this.products.find(product => product.id === productId);
         if (!product) {
             throw new Error('Producto no encontrado.');
         }
-        return product
+        return product;
     }
 }
 
 const productManager = new ProductManager();
 
-// Agregar algunos productos
-productManager.addProduct(new Product("Camisa", "Descripción de la camisa", 20, "imagen1.jpg", "ABC123", 10));
-productManager.addProduct(new Product("Pantalón", "Descripción del pantalón", 30, "imagen2.jpg", "DEF456", 15));
+try {
+    productManager.addProduct(new Product("Camisa", "Descripción de la camisa", 20, "imagen1.jpg", "ABC123", 10));
+    productManager.addProduct(new Product("Pantalón", "Descripción del pantalón", 30, "imagen2.jpg", "DEF456", 15));
 
-// Obtener todos los productos
-console.log('Todos los productos:', productManager.getProducts());
+    console.log('Todos los productos:', productManager.getProducts());
 
-// Obtener un producto por su ID
-const productId = productManager.getProducts()[0].id;
-console.log('Producto con ID', productId, ':', productManager.getProductById(productId));
+    const productId = productManager.getProducts()[0].id;
+    console.log('Producto con ID', productId, ':', productManager.getProductById(productId));
+} catch (error) {
+    console.error(error.message);
+}
